8981d8fb274dcab6c8cbe87f2e709888e6794dfd
TurtlePaw <turtlesimsyt@gmail.com>
2023-03-09 17:28:24 -0500

    delete dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './Util/Constants';
-export * from './DatabaseProviders/mapstorage';
-export * from './DatabaseProviders/mongoose';
-export * from './Core/TokenStorage';
-export * from './Core/Authorization';
-export * from './Core/Application';
-//# sourceMappingURL=index.d.ts.map
